c92e4fd903d9c5ed5968dd68e5245600cf6a407f
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-10-17 09:44:15 -0500

    minor edits

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,7 +25,6 @@ router.put("/api/burgers/:id", function (req, res) {
     devoured: true
   }, condition, function (result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -37,7 +36,6 @@ router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
   burger.deleteOne(condition, function (result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,34 +1,29 @@
 $(function () {
     $("#addburger").on("submit", function (event) {
-      // Make sure to preventDefault on a submit event.
       event.preventDefault();
       var validBurger = $("#burger").val().trim();
   
       if (validBurger.length > 0) {
-        //no blank names
         var newBurger = {
           burger: validBurger
         };
   
-        // Send the POST request.
         $.ajax("/api/burgers", {
           type: "POST",
           data: newBurger
         }).then(
           function () {
             console.log("added new burger");
-            // Reload the page to get the updated list
             location.reload();
           }
         );
-      }; // if (validBurger.length
+      }; 
     });
   
     $(".change-burger").on("click", function (event) {
   
       var id = $(this).data("burgerid");
   
-      // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: {
@@ -37,7 +32,6 @@ $(function () {
       }).then(
         function () {
           console.log("moved to devoured column");
-          // Reload the page to get the updated list
           location.reload();
         }
       );
@@ -47,7 +41,6 @@ $(function () {
   
       var id = $(this).data("burgerid");
   
-      // Send the DELETE request.
       $.ajax("/api/burgers/" + id, {
         type: "DELETE",
         dataType: 'text',
